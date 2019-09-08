7dc1b038e3d5ee8010a1356db812cb01ccd48e5d
masood <smasood@hotmail.com>
2019-09-08 15:03:36 -0400

    push build

---
+++ build/precache-manifest.be16ec1a55d1744484d70b52cb381893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc37e0a42dbea1a425be1600e30f14d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6ca473844a503259612",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "bd500913eae4b1acd6dd",
+    "url": "/static/js/2.ceea271f.chunk.js"
+  },
+  {
+    "revision": "d6ca473844a503259612",
+    "url": "/static/js/main.1c1ac5ae.chunk.js"
+  },
+  {
+    "revision": "a570b5ade460b4fb1681",
+    "url": "/static/js/runtime~main.5382e5a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
